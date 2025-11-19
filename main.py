de281897dd338fe9f791e5d649330fe82d853c05
jprincip21 <jprincip21@gmail.com>
2025-11-18 20:05:52 -0500

    Added page lines

--- main.py
+++ main.py
@@ -15,22 +15,21 @@ for index, row in df.iterrows(): # Iterrows returns a dictionary (order......*na
             pdf.set_font(family="Times", style="B", size=24) #Sets font to 24pt bolded times new roman
             pdf.set_text_color(100, 100, 100) # Makes text gray
             pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0) # Creates cell for pdf
-            pdf.line(10, 21, 200, 21)
-
             # Set up footer for header page
             pdf.ln(265)  # add 278 break lines to go to bottom of page
-
-            pdf.set_font(family="Times", style="I", size=8)
-            pdf.set_text_color(180, 180, 180)
-            pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
         else:
             # Set up footer for blank pages
             pdf.ln(277) # add 278 break lines to go to bottom of page
 
-            pdf.set_font(family="Times", style="I", size=8)
-            pdf.set_text_color(180, 180, 180)
-            pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Add page lines every 10 mm starting from 20mm down
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+
 # W = 0: entire page,
 # h=12: height is 12mm, Should be same size as font
 # txt is Text,
